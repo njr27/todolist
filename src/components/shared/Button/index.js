71371829b384407e1e58957c16b5416aa6f2bfd7
njr27 <nivaldojunior00@gmail.com>
2022-07-19 19:51:11 -0300

    Projeto em andamento

--- src/components/shared/Button/index.js
+++ src/components/shared/Button/index.js
@@ -6,12 +6,16 @@ importCss('./components/shared/Button/styles.css')
 export const Button = (props) =>{
 
     const _button = document.createElement('button')
-    _button.classList.add('button')
+    _button.classList.add('button', props.class)
     props?.width && (_button.style.width = props.width) 
     props?.height && (_button.style.height = props.height) 
 
     const _icon = Icon(props?.icon)
     props?.icon && _button.append(_icon)
+
+    
+    props?.text && (_button.textContent = props.text)
+
     
 
     return _button
--- src/index.js
+++ src/index.js
@@ -9,19 +9,30 @@ const $root = document.querySelector('#root')
 const input = Input({
     icon: 'search',
     placeholder: 'Digite sua pesquisa aqui',
-    width: '500px'
+    width: '440px'
 })
 
 const button = Button({
     icon: 'plus',
     width: '40px',
-    height: '40px'
+    height: '40px',
+    class: 'button-add'
+})
+
+const button2 = Button({    
+    width: '293px',
+    height: '40px',
+    text: 'Desfazer',
+    class: 'button-undo'
 })
 
 
 
-let container = Container({children: input})
+
+
+const container = Container({children: input})
 
 container.appendChild(button)
+container.appendChild(button2)
 
 $root.appendChild(container)
